9b6816d17639374bf351167c4f232233f2b9e98d
MDavid <davidmemba2@gmail.com>
2023-08-09 21:01:07 +0300

    Creating a new user interface

--- Index.js
+++ Index.js
@@ -1,22 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
-
+var warn = "";
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.render(__dirname + "/views/index.ejs");
+	res.render(__dirname + "/views/index.ejs", {warn});
 });
 
 app.get("/about", (req, res) => {
-	res.render(__dirname + "/views/about.ejs");
+		res.render(__dirname + "/views/file-editor.ejs");
+	
 });
 
-app.get("/dashboard", (req, res) =>{
-res.render(__dirname + "/views/dashboard.ejs");
+app.post("/dashboard", (req, res) =>{
+	var user_email = req.body["email"];
+	var user_password = req.body["password"];
+	warn = "You entered the wrong password";
+	if (user_password == "Mdavid5099"){
+		res.render(__dirname + "/views/dashboard.ejs");
+	}else{
+		res.render(__dirname + "/views/index.ejs", {warn});
+	}
+	
+
 })
 
 
