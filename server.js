9b493188c74b4e5291b8f4f6f1193ea506204adf
MDavid <davidmemba2@gmail.com>
2023-08-21 01:55:04 +0300

    Modify the site to have reusable components

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 var warn = "";
+var section = "";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,8 +14,22 @@ app.get("/", (req, res) => {
 	res.render(__dirname + "/views/index.ejs", {warn});
 });
 
-app.get("/about", (req, res) => {
-		res.render(__dirname + "/views/file-editor.ejs");
+app.post("/about", (req, res) => {
+		// res.render(__dirname + "/views/file-editor.ejs");
+		var email = req.body["email"];
+		var username = req.body["username"];
+		var user_password = req.body["password"];
+		warn = "You entered the wrong password";
+		// if (user_password == "Mdavid5099"){
+			res.render(__dirname + "/views/file-editor.ejs", {email, username, section});
+		// }else{
+		// 	res.render(__dirname + "/views/index.ejs", {warn});
+		// }
+		section = req.body["new-section"];		
+		
+
+
+
 	
 });
 
@@ -29,7 +44,7 @@ app.post("/dashboard", (req, res) =>{
 	}
 	
 
-})
+});
 
 
 app.listen(port, () => {
